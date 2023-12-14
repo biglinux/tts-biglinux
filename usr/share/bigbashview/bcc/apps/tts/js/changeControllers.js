95b26c52dacf6de3dc0ad1f57e1df096caa3f634
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-12-14 18:33:12 -0300

    feat: range input to volume

--- usr/share/bigbashview/bcc/apps/tts/js/changeControllers.js
+++ usr/share/bigbashview/bcc/apps/tts/js/changeControllers.js
@@ -7,9 +7,6 @@ const PITCHSelect = document.querySelector('#PITCH')
 const RATESelect = document.querySelector('#RATE')
 
 /** @type {HTMLSelectElement} */
-const VOLUMESelect = document.querySelector('#VOLUME')
-
-/** @type {HTMLSelectElement} */
 const VOICESelect = document.querySelector('#VOICE')
 
 PITCHSelect.addEventListener("change", () => {
@@ -20,10 +17,23 @@ RATESelect.addEventListener("change", () => {
     API.setRate(RATESelect.value)
 })
 
-VOLUMESelect.addEventListener("change", () => {
-    API.setVolume(VOLUMESelect.value)
-})
-
 VOICESelect.addEventListener("change", () => {
     API.setVoice(VOICESelect.value)
+})
+
+/** @type {HTMLInputElement} */
+const volumeRangeInput = document.querySelector("#volume-range")
+/** @type {HTMLInputElement} */
+const volumeInput = document.querySelector("#volume")
+
+const shouldChange = (input1, input2) => input1.value !== input2.value
+
+volumeRangeInput.addEventListener("input", () => {
+    if (shouldChange(volumeInput, volumeRangeInput)) {
+        volumeInput.value = volumeRangeInput.value
+    }
+})
+
+volumeRangeInput.addEventListener("change", () => {
+    API.setVolume(volumeRangeInput.value)
 })
